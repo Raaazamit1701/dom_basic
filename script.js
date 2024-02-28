bfe631275aeb367a611414ec062d697c65cf3d90
amit raj <amitraaaz0@gmial.com>
2024-02-28 21:14:35 +0530

    added dom

---
+++ script.js
@@ -0,0 +1,9 @@
+
+let target = document.getElementById("target");
+target.style.background = "yellow";
+target.style.margin = "20px";
+target.style.padding = "10px";
+target.style.fontSize = "18px";
+target.style.fontWeight = "bold";
+target.style.height = "200px";
+target.style.width ="300px";
